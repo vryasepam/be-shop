29f0acd729a7ec990c7b7de1dc0c6ddf2773f506
Volodymyr Yas <volodymyr_yas@epam.com>
2024-04-29 22:08:38 +0300

    feat(task-5): added lambdas for task 5

---
+++ services/import-service/handlers/importProductsFile.js
@@ -0,0 +1,27 @@
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+
+export const handler = async (event, context, cb) => {
+  const { name } = event.queryStringParameters;
+
+  const params = {
+    Bucket: process.env.AWS_S3_BUCKET,
+    ContentType: 'text/csv',
+    Key: `uploaded/${name}`,
+  };
+
+  const s3Client = new S3Client({ region: 'eu-east-1' });
+
+  const signedUrl = await getSignedUrl(s3Client, new PutObjectCommand(params), {
+    expiresIn: 60,
+  });
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+    },
+    body: JSON.stringify(signedUrl),
+  };
+};
